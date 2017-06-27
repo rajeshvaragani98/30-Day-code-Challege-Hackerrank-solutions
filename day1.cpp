cfc7d0011779c37798edb89f6d7d5e3b5c068f66
Rajesh Varagani <rajeshvaragani12345@gmail.com>
2017-06-27 15:21:14 +0530

    Create day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,20 @@
+    int i2;
+    double d2;
+    string s2;
+
+    
+ string tmp;
+
+    getline(cin, tmp);
+    i2 = stoi(tmp);
+
+    getline(cin, tmp);
+    d2 = stod(tmp);
+
+    getline(cin, s2);    
+
+        printf("%i\n", i + i2);
+
+        printf("%.1f\n", d + d2);
+
+        cout << s + s2 << endl;
